03e5c4f3fe59213cc46d60fca1a3aba6efcefe51
Dr. Seek <75817900+dreamy32@users.noreply.github.com>
2022-12-08 13:44:51 -0500

    écrire les méthodes du zoom 11 novembre
    Tas une méthode ou des méthodes qui avaient le meme nom que ceux que je devais mettre. Je les ai mit en commentaires au lieu de les effacer.
    Je t'en avertis ici pour que tu prennes ça en considération avant de continuer.

--- wwwroot/ImagesManager2022/js/api.js
+++ wwwroot/ImagesManager2022/js/api.js
@@ -2,7 +2,10 @@ const baseUrl = "http://localhost:5000"; // remplacer quand host sur glitch
 const apiImageURL = `${baseUrl}/api/images`;
 const apiAccountsURL = `${baseUrl}/accounts`;
 const apiTokenURL = `${baseUrl}/token`;
-
+//Storage Keys
+const accessTokenKey = 'access_Token';
+const userKey = 'user';
+//
 function HEAD(successCallBack, errorCallBack) {
   $.ajax({
     url: apiImageURL,
@@ -10,6 +13,168 @@ function HEAD(successCallBack, errorCallBack) {
     contentType: "text/plain",
     complete: (request) => {
       successCallBack(request.getResponseHeader("ETag"));
+    },
+    error: function (jqXHR) {
+      errorCallBack(jqXHR.status);
+    },
+  });
+}
+function POST(data, successCallBack, errorCallBack) {
+  $.ajax({
+    url: apiImageURL,
+    type: "POST",
+    contentType: "application/json",
+    data: JSON.stringify(data),
+    success: (data) => {
+      successCallBack(data);
+    },
+    error: function (jqXHR) {
+      errorCallBack(jqXHR.status);
+    },
+  });
+}
+
+function PUT(bookmark, successCallBack, errorCallBack) {
+  $.ajax({
+    url: apiImageURL + "/" + bookmark.Id,
+    type: "PUT",
+    contentType: "application/json",
+    data: JSON.stringify(bookmark),
+    success: () => {
+      successCallBack();
+    },
+    error: function (jqXHR) {
+      errorCallBack(jqXHR.status);
+    },
+  });
+}
+function DELETE(id, successCallBack, errorCallBack) {
+  $.ajax({
+    url: apiImageURL + "/" + id,
+    type: "DELETE",
+    success: () => {
+      successCallBack();
+    },
+    error: function (jqXHR) {
+      errorCallBack(jqXHR.status);
+    },
+  });
+}
+/*Local Storage utilities functions */
+function tokenRequestURL() {
+  return apiTokenURL;
+}
+function storeAccessToken(token) {
+  sessionStorage.setItem(accessTokenKey, token);
+}
+function eraseAccessToken() {
+  sessionStorage.removeItem(accessTokenKey);
+}
+function retrieveAccessToken() {
+  return sessionStorage.getItem(accessTokenKey);
+}
+function getBearerAuthorizationToken() {
+  return { 'Authorization': `Bearer ${retrieveAccessToken()}` };
+}
+function storeLoggedUser(user) {
+  sessionStorage.setItem(userKey, JSON.stringify(user));
+}
+function retrieveLoggedUser() {
+  return JSON.parse(sessionStorage.getItem(userKey));
+}
+function deConnect() {
+  sessionStorage.removeItem(userKey);
+  eraseAccessToken();
+}
+// function StoreUserInfo(user) {
+//   sessionStorage.setItem("user", JSON.stringify(user));
+// }
+
+// function GetUserInfo() {
+//   return JSON.parse(sessionStorage.getItem(userKey));
+// }
+/*AJAX functions utilities*/
+function POST_REGISTER(data, successCallBack, errorCallBack) {
+  $.ajax({
+    url: apiAccountsURL + "/register",
+    type: "POST",
+    contentType: "application/json",
+    data: JSON.stringify(data),
+    success: (data) => {
+      successCallBack(data);
+      StoreUserInfo(data);
+    },
+    error: function (jqXHR) {
+      errorCallBack(jqXHR.status);
+    },
+  });
+}
+function POST_LOGIN(Email, Password, successCallBack, errorCallBack) {
+  $.ajax({
+    url: apiTokenURL,
+    contentType: "application/json",
+    type: "POST",
+    data: JSON.stringify({ Email, Password }),
+    success: (profil) => {
+      storeAccessToken(profil.Access_token);
+
+    },
+    error: function (jqXHR) {
+      errorCallBack(jqXHR.status);
+    },
+  });
+}
+
+function GET_VERIFY(code, successCallBack, errorCallBack) {
+  $.ajax({
+    url: apiAccountsURL + "/verify?id=" + GetUserInfo().Id + "&code=" + code,
+    type: "GET" //,
+    //contentType: 'application/json',
+    //data: JSON.stringify(data),
+    //success: (data) => { successCallBack(data) },
+    //error: function (jqXHR) { errorCallBack(jqXHR.status) }
+  });
+}
+function GET_UserInfo(userId, successCallBack, errorCallBack) {
+  $.ajax({
+    url: `${apiAccountsURL}/index` + "/" + userId,
+    type: "GET",
+    contentType: "text/plain",
+    data: {},
+    success: (profil) => {
+      console.log(profil);
+      storeLoggedUser(profil);
+      successCallBack(profil);
+    },
+    error: function (jqXHR) {
+      errorCallBack(jqXHR.status);
+    },
+  });
+}
+function modify_UserInfo(userInfo, successCallBack, errorCallBack) {
+  $.ajax({
+    url: `${apiAccountsURL}/modify` + "/" + userInfo.Id,
+    type: "PUT",
+    contentType: "application/json",
+    headers: getBearerAuthorizationToken(),
+    data: JSON.stringify(userInfo),
+    success: () => {
+      GET_UserInfo(userInfo.Id, successCallBack, error);
+    },
+    error: function (jqXHR) {
+      errorCallBack(jqXHR.status);
+    },
+  });
+}
+function logout(userId, successCallBack, errorCallBack) {
+  $.ajax({
+    url: `${apiAccountsURL}/logout` + "/" + userId,
+    type: "GET",
+    data: {},
+    headers: getBearerAuthorizationToken(),
+    success: () => {
+      deConnect();
+      successCallBack();
     },
     error: function (jqXHR) {
       errorCallBack(jqXHR.status);
@@ -35,95 +200,6 @@ function GET_ALL(successCallBack, errorCallBack, queryString = null) {
     type: "GET",
     success: (data, status, xhr) => {
       successCallBack(data, xhr.getResponseHeader("ETag"));
-    },
-    error: function (jqXHR) {
-      errorCallBack(jqXHR.status);
-    },
-  });
-}
-function POST(data, successCallBack, errorCallBack) {
-  $.ajax({
-    url: apiImageURL,
-    type: "POST",
-    contentType: "application/json",
-    data: JSON.stringify(data),
-    success: (data) => {
-      successCallBack(data);
-    },
-    error: function (jqXHR) {
-      errorCallBack(jqXHR.status);
-    },
-  });
-}
-function POST_LOGIN(data, successCallBack, errorCallBack) {
-  $.ajax({
-    url: `${apiTokenURL}`,
-    type: "POST",
-    contentType: "application/json",
-    data: JSON.stringify(data),
-    success: (data) => {
-      successCallBack(data);
-    },
-    error: function (jqXHR) {
-      errorCallBack(jqXHR.status);
-    },
-  });
-}
-function POST_REGISTER(data, successCallBack, errorCallBack) {
-  $.ajax({
-    url: apiAccountsURL + "/register",
-    type: "POST",
-    contentType: "application/json",
-    data: JSON.stringify(data),
-    success: (data) => {
-      successCallBack(data);
-      StoreUserInfo(data);
-    },
-    error: function (jqXHR) {
-      errorCallBack(jqXHR.status);
-    },
-  });
-}
-
-function StoreUserInfo(user) {
-  sessionStorage.setItem("user", JSON.stringify(user));
-}
-
-function GetUserInfo() {
-  return JSON.parse(sessionStorage.getItem("user"));
-}
-
-function GET_VERIFY(code,successCallBack, errorCallBack) {
-  $.ajax({
-    url: apiAccountsURL + "/verify?id=" + GetUserInfo().Id + "&code=" + code,
-    type: "GET" //,
-    //contentType: 'application/json',
-    //data: JSON.stringify(data),
-    //success: (data) => { successCallBack(data) },
-    //error: function (jqXHR) { errorCallBack(jqXHR.status) }
-  });
-}
-
-function PUT(bookmark, successCallBack, errorCallBack) {
-  $.ajax({
-    url: apiImageURL + "/" + bookmark.Id,
-    type: "PUT",
-    contentType: "application/json",
-    data: JSON.stringify(bookmark),
-    success: () => {
-      successCallBack();
-    },
-    error: function (jqXHR) {
-      errorCallBack(jqXHR.status);
-    },
-  });
-}
-function DELETE(id, successCallBack, errorCallBack) {
-  $.ajax({
-    url: apiImageURL + "/" + id,
-    type: "DELETE",
-    success: () => {
-      successCallBack();
     },
     error: function (jqXHR) {
       errorCallBack(jqXHR.status);
